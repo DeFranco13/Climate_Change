de83dbbf16866729b09de8a843dd6ef5ddcfcb07
jordi <jordifransen@hotmail.com>
2023-01-14 12:17:17 +0100

    Ramp 1 lvl1

--- Assets/Level 1/Script/Ramp.cs
+++ Assets/Level 1/Script/Ramp.cs
@@ -1,18 +1,45 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Ramp : MonoBehaviour
+public class Ramp : TickMultiplier
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+
+    int trigger1 = 800;
+    int trigger2 = 1000;
+    int trigger3 = 1400;
+
+
+
+    void rise1() { 
+    
     }
+
+    void rise2()
+    {
+
+    }
+
+    void rise3()
+    {
+
+    }
+
+    
+    
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(trigger1 <= TickObject.instance.TotalCO2 || trigger2 > TickObject.instance.TotalCO2){
+
+            rise1();
+
+        }else if (trigger2 <= TickObject.instance.TotalCO2 || trigger3 > TickObject.instance.TotalCO2){
+
+            rise2();
+
+        }else if(trigger3 <= TickObject.instance.TotalCO2){
+
+            rise3();
+        }
     }
 }

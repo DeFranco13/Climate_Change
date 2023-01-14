3b3fa48ecd127e6e0828e80b5812219e7b3b32cc
Noah Pladys <pladys.noah@gmail.com>
2023-01-14 16:50:24 +0100

    Level 4 forest fire

--- Assets/Level 4/Scripts/ForestFire.cs
+++ Assets/Level 4/Scripts/ForestFire.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class ForestFire : TickMultiplierScript
 {
+    public GameObject fire;
+
+    public void Start()
+    {
+        base.Start();
+    }
+
     public override void Do()
     {
-        
+        fire.active = true;
     }
 
     public override void Undo()
     {
-        
+        fire.active = false;
     }
 }
